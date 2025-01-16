bd0580aa8ec6156283bb768360c79450e16a6855
lana <svetlasosnova@gmail.com>
2025-01-16 17:48:40 +0100

    Player + camera movement done

--- BusinessProject/Assets/ExternalPacks/PlayerControllerPack/Scripts/CameraConroller.cs
+++ BusinessProject/Assets/ExternalPacks/PlayerControllerPack/Scripts/CameraConroller.cs
@@ -6,28 +6,35 @@ using UnityEngine.Serialization;
 
 public class CameraConroller : MonoBehaviour
 {
-    public float topClamp = -90f;
-    public float bottomClamp = 90f;
-    public float verticalSensitivity = 500f;
-    private float xRotation = 0f;
+    [SerializeField] private GameObject target;
+    
+    [Range(0f, 90f)] [SerializeField] private float cameraPitch;
+    [Range(-5f, 0f)] [SerializeField] private float zCameraOffset;
+    [Range(0f, 5f)] [SerializeField] private float yCameraOffset;
+    [SerializeField] [Range(300f, 600f)] private float horizontalSensitivity = 500f;
+    
+    private float yRotation = 0f;
     
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        transform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
     }
 
     private void Update()
     {
-        RotateCamera();
+        if(target) RotateCamera();
     }
 
     void RotateCamera()
     {
-        float mouseY = Input.GetAxis("Mouse Y") * verticalSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
+        yRotation += mouseX;
         
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
-        
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        Quaternion newRotation = Quaternion.Euler(cameraPitch, yRotation, 0);
+        Vector3 newDistanceVec = new Vector3(0.0f, yCameraOffset, zCameraOffset);
+        Vector3 newPosition = newRotation * newDistanceVec;
+        transform.rotation = newRotation;
+        transform.position = newPosition + target.transform.position;
     }
 }
--- BusinessProject/Assets/ExternalPacks/PlayerControllerPack/Scripts/PlayerMovement.cs
+++ BusinessProject/Assets/ExternalPacks/PlayerControllerPack/Scripts/PlayerMovement.cs
@@ -5,80 +5,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float jumpForce = 5f;
-    [SerializeField] private float moveForce = 5f;
     [SerializeField] private float moveSpeed = 5f;
 
-    private enum ControlType
-    {
-        Force,
-        Velocity
-    }
-    private ControlType control;
-    private float groundCheckRadius = 0.5f;
     private Rigidbody playerRigidbody;
     
-    private float yRotation = 0f;
-    public float horizontalSensitivity = 500f;
-    
-    // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    private void Update()
-    {
-        if (IsGrounded()) {
-            //Debug.Log("grounded");
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                playerRigidbody.velocity += new Vector3(0, jumpForce, 0);
-            }
-        }
-        
-        RotateY();
-    }
-
     private void FixedUpdate()
     {
-        Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Create movement vector
+        Vector3 moveVector = new Vector3(horizontal, 0, vertical).normalized;
+        if (moveVector == Vector3.zero) return;
         
-        control = IsGrounded() ? ControlType.Velocity : ControlType.Force;
+        // Convert input vector to camera-relative movement
+        Vector3 cameraForward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
+        Vector3 cameraRight = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
+
+        // Calculate the relative movement direction
+        Vector3 relativeMoveDirection = (cameraRight * moveVector.x + cameraForward * moveVector.z).normalized;
         
-        switch (control) {
-            case ControlType.Force:
-                playerRigidbody.AddRelativeForce(moveVector * moveForce);
-                break;
-            case ControlType.Velocity:
-                Vector3 newVelocity = playerRigidbody.transform.right * moveVector.x +
-                                      playerRigidbody.transform.forward * moveVector.z;
-                newVelocity *= moveSpeed;
-                newVelocity.y = playerRigidbody.velocity.y;
-                playerRigidbody.velocity = newVelocity;
-                break;
-        }
-    }
+        // Apply movement
+        Vector3 newVelocity = relativeMoveDirection * moveSpeed;
+        newVelocity.y = playerRigidbody.velocity.y;
+        
+        //playerRigidbody.velocity = newVelocity;
+        transform.Translate(relativeMoveDirection * moveSpeed * Time.deltaTime, Space.World);
+        
+        // Rotate the player to face the relative movement direction
+        float targetAngle = Mathf.Atan2(relativeMoveDirection.x, relativeMoveDirection.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+        //
+        // // Rotate the player to face movement direction, including diagonals
+        // float angle = Mathf.Atan2(moveVector.x, moveVector.z) * Mathf.Rad2Deg; // Angle in degrees
+        // float snappedAngle = Mathf.Round(angle / 45f) * 45f;
+        //transform.rotation = Quaternion.Euler(0, snappedAngle, 0);
+        transform.rotation = targetRotation;
 
-    bool IsGrounded()
-    {
-        return Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-        //Debug.DrawRay(groundCheck.position, Vector3.down * groundCheckRadius, Color.magenta);
-    }
-
-    private void RotateY()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
-        yRotation += mouseX;
-        transform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
     }
 }
