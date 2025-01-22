70228a77c49024fe8fd593c82e088cfad737ac20
HangyBoi <6avuha228@gmail.com>
2025-01-22 11:40:10 +0100

    Final Commit before Merge
    ...

--- BusinessProject/Assets/Scripts/StateMachine/EnemyStates/RoamingStateEnemy.cs
+++ BusinessProject/Assets/Scripts/StateMachine/EnemyStates/RoamingStateEnemy.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(TargetDetector))]
 [RequireComponent(typeof(MoveBehaviour))]
 public class RoamingStateEnemy : State
