1a3e6c19ea9caba0e731d8e0a2e1f5c04684b861
froostel <marius.a.irimia@gmail.com>
2025-01-28 18:11:42 +0100

    More sound implementation and making of Level 1
    - Changed the Leshling and Player Steps.
    - Added Auka passive noise to make them easier to find.
    - Started making lvl 1 (90% done, just needs enemies).
    - Altered Logs and Rocks prefabs to have capsule colliders and removed colliders from mushrooms.
    - Re-added the previous camera script to player for the A/B Test. Can be removed.

--- BusinessProject/Assets/Scripts/Player/Camera_Controller_ABTest.cs
+++ BusinessProject/Assets/Scripts/Player/Camera_Controller_ABTest.cs
@@ -13,8 +13,11 @@ public class Camera_Controller_ABTest : MonoBehaviour
     [SerializeField][Range(300f, 600f)] private float horizontalSensitivity = 500f;
 
     private float yRotation = 0f;
+
     //personally added
-    private float rotationAmmount = 90f;
+    [SerializeField] private float rotationAmmount = 90f;
+    [SerializeField] private float smoothSpeed = 5f;
+    public bool lockedCam = false;
 
     void Start()
     {
@@ -29,24 +32,43 @@ public class Camera_Controller_ABTest : MonoBehaviour
 
     void RotateCamera()
     {
-        //float mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
-        //yRotation += mouseX;
+        //Free cam
 
-        //personally added
-        if (Input.GetKeyDown(KeyCode.E))
+        if (!lockedCam)
         {
-            yRotation += rotationAmmount;
+            float mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
+            yRotation += mouseX;
+
+            Quaternion newRotation = Quaternion.Euler(cameraPitch, yRotation, 0);
+            Vector3 newDistanceVec = new Vector3(0.0f, yCameraOffset, zCameraOffset);
+            Vector3 newPosition = newRotation * newDistanceVec;
+            transform.rotation = newRotation;
+            transform.position = newPosition + target.transform.position;
         }
 
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            yRotation -= rotationAmmount;
-        }
 
-        Quaternion newRotation = Quaternion.Euler(cameraPitch, yRotation, 0);
-        Vector3 newDistanceVec = new Vector3(0.0f, yCameraOffset, zCameraOffset);
-        Vector3 newPosition = newRotation * newDistanceVec;
-        transform.rotation = newRotation;
-        transform.position = newPosition + target.transform.position;
+
+        //Locked Cam
+
+        if (lockedCam)
+        {
+            //personally added
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                yRotation += rotationAmmount;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                yRotation -= rotationAmmount;
+            }
+
+            Quaternion targetRotation = Quaternion.Euler(cameraPitch, yRotation, 0);
+            Vector3 targetDistanceVec = new Vector3(0.0f, yCameraOffset, zCameraOffset);
+            Vector3 targetPosition = targetRotation * targetDistanceVec + target.transform.position;
+            // Smoothly interpolate to the target position and rotation
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smoothSpeed * Time.deltaTime);
+        }
     }
 }
