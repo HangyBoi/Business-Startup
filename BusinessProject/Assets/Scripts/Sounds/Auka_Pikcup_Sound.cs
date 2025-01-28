60e9ce9e942605be780d7f85fc2b9370538676f3
froostel <marius.a.irimia@gmail.com>
2025-01-28 18:13:56 +0100

    Removed the AB test cam script
    script removed

--- BusinessProject/Assets/Scripts/Sounds/Auka_Pikcup_Sound.cs
+++ BusinessProject/Assets/Scripts/Sounds/Auka_Pikcup_Sound.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Auka_Pikcup_Sound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+    [Range(0f, 1f)]
+    public float clipVolume = 1.0f;
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+
+        //choose random clip
+        AudioClip randomClip = audioClips[Random.Range(0, audioClips.Length)];
+
+        //play audio clip
+        audioSource.PlayOneShot(randomClip, clipVolume);
+
     }
 }
+
