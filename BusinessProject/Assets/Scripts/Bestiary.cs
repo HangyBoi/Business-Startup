f18ac4cbe834c603aafae55e39f3e50bbc388bdb
lana <svetlasosnova@gmail.com>
2025-01-29 15:23:01 +0100

    Bestiary Done

---
+++ BusinessProject/Assets/Scripts/Bestiary.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Add this namespace for UnityEngine.UI.Image
+
+public class Bestiary : MonoBehaviour
+{
+    [SerializeField] private GameObject bestiaryImage;
+    [SerializeField] private GameObject buttonNext;
+    [SerializeField] private GameObject buttonPrevious;
+
+    [SerializeField] private Sprite[] pages; // Change to Sprite if using UnityEngine.UI.Image
+    private int currentPage = 0; // To keep track of the current page
+
+    void Start()
+    {
+        if (pages.Length != 0)
+        {
+            Image imageComponent = bestiaryImage.GetComponent<Image>();
+            if (imageComponent != null)
+            {
+                imageComponent.sprite = pages[0];
+            }
+            else
+            {
+                Debug.LogError("No Image component found on bestiaryImage GameObject.");
+            }
+        }
+        //Optionally disable UI elements initially
+        bestiaryImage.SetActive(false);
+        buttonNext.SetActive(false);
+        buttonPrevious.SetActive(false);
+    }
+
+    public void ManageBestiary()
+    {
+        bool isActive = bestiaryImage.activeInHierarchy;
+        bestiaryImage.SetActive(!isActive);
+        buttonNext.SetActive(!isActive);
+        buttonPrevious.SetActive(!isActive);
+    }
+
+    // Example methods for navigating pages
+    public void NextPage()
+    {
+        if (currentPage < pages.Length - 1)
+        {
+            currentPage++;
+            UpdatePage();
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if (currentPage > 0)
+        {
+            currentPage--;
+            UpdatePage();
+        }
+    }
+
+    private void UpdatePage()
+    {
+        Image imageComponent = bestiaryImage.GetComponent<Image>();
+        if (imageComponent != null)
+        {
+            imageComponent.sprite = pages[currentPage];
+        }
+    }
+}
