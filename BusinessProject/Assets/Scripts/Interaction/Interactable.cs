c7b7b791d4641d063f4639af21f1f1b28e54238c
HangyBoi <6avuha228@gmail.com>
2025-01-30 14:26:59 +0100

    Dialogues Finishing for Main Gamelopp
    Done

--- BusinessProject/Assets/Scripts/Interaction/Interactable.cs
+++ BusinessProject/Assets/Scripts/Interaction/Interactable.cs
@@ -32,47 +32,4 @@ public abstract class Interactable : MonoBehaviour
         return interactionData.interactableName;
     }
 
-    // #region Interaction Handlers
-    //
-    // private void HandlePickup()
-    // {
-    //     //Debug.Log("Handle Pickup");
-    //     //if(!TaskManager.taskManager.GetCurrentTask()) Debug.LogAssertion("No current task in task manager!");
-    //     if(TaskManager.taskManager.GetCurrentTask() && interactableID == TaskManager.taskManager.GetCurrentTask().Data.TaskID) TaskManager.taskManager.UpdateTaskProgress(interactableID, 1);
-    //     // else
-    //     // {
-    //     //     Debug.Log("No task related to this pickup id!");
-    //     // }
-    //     
-    //     if(Inventory.inventory) Inventory.inventory.AddItem(this);
-    //     else
-    //     {
-    //         Debug.Log("Inventory not created!");
-    //     }
-    //     if (interactionData is PickupInteractionData pIntData && pIntData.destroyOnInteract)
-    //     {
-    //         Destroy(gameObject);
-    //     }
-    //     else
-    //     {
-    //         gameObject.SetActive(false);
-    //     }
-    // }
-    //
-    // private void HandleMap()
-    // {
-    //     //Debug.Log("Handle Map");
-    //     if(UIManager.uiManager.mapUiController) UIManager.uiManager.mapUiController.ShowMap();
-    //     TestAcceptTask();
-    // }
-    //
-    // #endregion
-    //
-    // /// <summary>
-    // /// Delayed test method to accept a task after initialization.
-    // /// </summary>
-    // private void TestAcceptTask()
-    // {
-    //     TaskManager.taskManager.AcceptTask("0");
-    // }
 }
