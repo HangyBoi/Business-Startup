5e3784378c73283e54cd9b45dbf5909b03238f6b
lana <svetlasosnova@gmail.com>
2025-01-30 16:24:30 +0100

    Background for UI

--- BusinessProject/Assets/Scripts/Bestiary.cs
+++ BusinessProject/Assets/Scripts/Bestiary.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI; // Add this namespace for UnityEngine.UI.Image
 
 public class Bestiary : MonoBehaviour
 {
     [SerializeField] private GameObject bestiaryImage;
-    [SerializeField] private GameObject buttonNext;
-    [SerializeField] private GameObject buttonPrevious;
-    [SerializeField] private GameObject sprite1;
-    [SerializeField] private GameObject sprite2;
+    [SerializeField] private List<GameObject> objectsToHide;
 
     [SerializeField] private Sprite[] pages; // Change to Sprite if using UnityEngine.UI.Image
     private int currentPage = 0; // To keep track of the current page
@@ -28,25 +26,22 @@ public class Bestiary : MonoBehaviour
                 Debug.LogError("No Image component found on bestiaryImage GameObject.");
             }
         }
-        //Optionally disable UI elements initially
-        bestiaryImage.SetActive(false);
-        buttonNext.SetActive(false);
-        buttonPrevious.SetActive(false);
         
-        if(sprite1 && sprite2) sprite1.SetActive(false); sprite2.SetActive(false);
+        if(objectsToHide.Count!=0)
+            foreach (GameObject obj in objectsToHide)
+            {
+                obj.SetActive(false);
+            }
     }
 
     public void ManageBestiary()
     {
         bool isActive = bestiaryImage.activeInHierarchy;
-        bestiaryImage.SetActive(!isActive);
-        buttonNext.SetActive(!isActive);
-        buttonPrevious.SetActive(!isActive);
-        if (sprite1 && sprite2)
-        {
-            sprite1.SetActive(!isActive); 
-            sprite2.SetActive(!isActive);
-        }
+        if(objectsToHide.Count!=0)
+            foreach (GameObject obj in objectsToHide)
+            {
+                obj.SetActive(!isActive);
+            }
     }
 
     // Example methods for navigating pages
